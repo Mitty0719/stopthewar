c4dfcf77b005361c7e6ddad8cf478202724d6a1d
Mitty0719 <sprout071900@gmail.com>
2022-03-08 16:22:04 +0900

    Add flag canvas animation

--- js/flag-section.js
+++ js/flag-section.js
@@ -1,5 +1,63 @@
 export class flagSection{
   constructor(){
-    console.log('hi');
+    this.section = document.querySelector('.flag-section');
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.section.appendChild(this.canvas);
+
+    this.screenRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    this.resize();
+    this.topBlock = new Block(this.stageWidth, this.stageHeight, true);
+    this.bottomBlock = new Block(this.stageWidth, this.stageHeight, false);
+
+    this.reqId = requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize(){
+    this.stageWidth = window.innerWidth;
+    this.stageHeight = window.innerHeight;
+
+    this.canvas.width = this.stageWidth * this.screenRatio;
+    this.canvas.height = this.stageHeight * this.screenRatio;
+    this.ctx.scale(this.screenRatio, this.screenRatio);
+  }
+
+  animate(){
+    this.reqId = requestAnimationFrame(this.animate.bind(this));
+    this.topBlock.draw(this.ctx);
+    this.bottomBlock.draw(this.ctx);
+
+    if(this.topBlock.x === 0 && this.bottomBlock.x === 0){
+      cancelAnimationFrame(this.reqId);
+    }
+  }
+}
+
+class Block{
+  constructor(stageWidth, stageHeight, isTop){
+    this.width = stageWidth;
+    this.height = stageHeight / 2;
+
+    if(isTop){
+      this.x = this.width;
+      this.y = 0;
+      this.color = '#005BBB';
+      this.vx = -1;
+    }else{
+      this.x = -this.width;
+      this.y = this.height;
+      this.color = '#FFD500';
+      this.vx = 1;
+    }
+  }
+
+  draw(ctx){
+    ctx.save();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.restore();
+    this.x += this.vx;
+    console.log('call');
   }
 }
